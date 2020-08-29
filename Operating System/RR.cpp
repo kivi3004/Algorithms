ea768ac30b53691ba0932c091422ebb28ce04404
Kirty Vijay <66775252+kivi3004@users.noreply.github.com>
2020-08-30 01:52:07 +0530

    Round Robin

--- Operating System/RR.cpp
+++ Operating System/RR.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 
 using namespace std;
+
 vector<int> waiting_Time(int n, int quantum, vector<int>& bt_time){
     vector<int> rm_time = bt_time;
     vector<int> wt_time(n);
@@ -27,31 +28,35 @@ vector<int> waiting_Time(int n, int quantum, vector<int>& bt_time){
     }
     return wt_time;
 }
+
 vector<int> turn_Around_time(int n, int quantum, vector<int>& wt_time, vector<int>& bt_time){
     vector<int> tat(n);
     for(int i=0; i<n; i++)
         tat[i] = wt_time[i]+ bt_time[i];
     return tat;
 }
+
 void average_Time(int n, int quantum, vector<int>& process, vector<int>& bt_time){
     vector<int> wt_time = waiting_Time(n, quantum, bt_time);
     vector<int> tat = turn_Around_time(n, quantum, wt_time, bt_time);
     int total_tat=0, total_wt=0;
-    cout<<"Processes   ||  Burst_Time   ||  Waiting Time  ||  Turn Around Time  "<<endl;
+    cout<<"Processes   ||  Burst_Time   ||  Waiting Time  ||  Turn Around Time  "<<endl;  
     for(int i=0; i<n; i++){
-        cout<<"    "<<process[i]<<"  ||     "<<bt_time[i]<<"    ||    "<<wt_time[i]<<"    ||   "<<tat[i]<<endl;
+        cout<<"    "<<process[i]<<"      ||     "<<bt_time[i]<<"    ||      "<<wt_time[i]<<"        ||   "<<tat[i]<<endl;
         total_tat += tat[i];
-        total_wt += wt_time[i];
+        total_wt += wt_time[i]; 
     }
     cout<<"\n\n";
     cout<<"Average Waiting Time : "<<(float)total_wt/(float)n<<endl;
     cout<<"Average Turn Around Time : "<<(float)total_tat/(float)n<<endl;
 }
+
 int main(){
     int quantum;
     cout<<"Enter quantum time : ";
     cin>>quantum;
     int n;
+    cout<<"Enter Number of Processes : ";
     cin>>n;
     vector<int> process(n);
     vector<int> burst_time(n);
