abe000808a7681e38a52a106380986d1e0cc9368
Kirty Vijay <66775252+kivi3004@users.noreply.github.com>
2020-09-01 20:38:57 +0530

    First_Come_First_Serve(With Different Arrival Time)

--- Operating System/First_come_first_serve.java
+++ Operating System/First_come_first_serve.java
@@ -1,64 +1,58 @@
-
-//import java.text.ParseException;
-import java.util.Scanner;
-class First_come_first_serve {
-    static void findWaitingTime(int processes[], int n, int bt[], int wt[], int at[]) {
-        wt[0] = 0+at[0];
-        for (int i = 1; i < n; i++) {
-            wt[i] = bt[i - 1] + wt[i - 1] - at[i] + at[i-1];
+#include <bits/stdc++.h>
+using namespace std;
+void print(vector<vector<int>>& pro){
+    cout<<"Process  Arrival Time  Burst Time  WAiting Time  Turn Around Time   Completion Time\n";
+    for(int i=0; i<pro.size(); i++){
+        for(int j=0; j<pro[i].size(); j++)
+            cout<<pro[i][j]<<"             ";
+        cout<<endl;
+    }
+    cout<<endl;
+}
+void findWaitingTime(vector<vector<int>>& pro, int n)
+{
+    int bt=0;
+    pro[0].push_back(0);
+    for (int i = 1; i < n; i++)
+    {
+        bt = (pro[i][1] > bt+pro[i-1][2] ) ? 0 : (bt+pro[i-1][2]-(pro[i][1]-pro[i-1][1]));
+        pro[i].push_back(bt);
+    
+    }
+}
+void findTurnAroundTime(vector<vector<int>>& pro, int n)
+{  
+    for (int i = 0; i < n; i++)
+    {
+        pro[i].push_back(pro[i][2] + pro[i][3]);
+        pro[i].push_back(pro[i][4] + pro[i][1]);
+    }
+}
+int main()
+{
+    int n;
+    cout << "Enter Number Of Process :";
+    cin >> n;
+    vector<vector<int>> pro;
+    for (int i = 1; i <= n; i++)
+    {
+        vector<int> v(3);     /*Process     ArrivalTime    BurstTime*/
+        cout << "Enter process " << i << " Arrival Time and Burst Time:";
+        v[0] = i;
+        cin >> v[1];
+        cin >> v[2];
+        if (pro.empty())
+            pro.push_back(v);
+        else
+        {
+            int j = pro.size() - 1;
+            while (j >= 0 && pro[j][1] >= v[1])   /*Sorting Processes according to arrival time*/
+                j--;
+            pro.insert(begin(pro) + j + 1, v);
         }
     }
-    static void findTurnAroundTime(int processes[], int n, int bt[], int wt[], int tat[]) {
-        for (int i = 0; i < n; i++) {
-            tat[i] = bt[i] + wt[i];
-        }
-    }
-    static void inputProcess(int processes[], int n, int bt[], int at[]){
-        Scanner sc = new Scanner(System.in);
-        for(int i=0; i<n; i++){
-            processes[i] =i+1;
-            int x = sc.nextInt();
-            bt[i]=x;
-            x = sc.nextInt();
-            at[i] = x;
-        }
-    }
-    static void findavgTime(int processes[], int n, int bt[], int at[]) {
-        int wt[] = new int[n], tat[] = new int[n];
-        int total_wt = 0, total_tat = 0;
-
-        findWaitingTime(processes, n, bt, wt, at);
-
-        //Function to find turn around time for all processes
-        findTurnAroundTime(processes, n, bt, wt, tat);
-
-        //Display processes along with all details
-        System.out.printf("Processes || Burst time || Arrival Time || Waiting time || Turn around time || Completion Time\n");
-        for (int i = 0; i < n; i++) {
-            total_wt = total_wt + wt[i];
-            total_tat = total_tat + tat[i];
-            System.out.printf("    %d     ||", processes[i]);
-            System.out.printf("   	%d       ||", bt[i]);
-            System.out.printf("    %d         ||", at[i]);
-            System.out.printf("       %d      ||", wt[i]);
-            System.out.printf("	   %d       ||", tat[i]);
-            System.out.printf("	   %d       \n", tat[i]+at[i]);
-        }
-        float s = (float)total_wt /(float) n;
-        int t = total_tat / n;
-        System.out.printf("Average waiting time = %f", s);
-        System.out.printf("\n");
-        System.out.printf("Average turn around time = %d ", t);
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int processes[] = new int[n];
-        int burst_time[] = new int[n];
-        int arrival_time[] = new int[n];
-        inputProcess(processes, n, burst_time, arrival_time);
-        findavgTime(processes, n, burst_time, arrival_time);
-
-    }
+    findWaitingTime(pro, n);
+    findTurnAroundTime(pro, n);
+    sort(begin(pro), end(pro));
+    print(pro);
 }
