0ea0d303a1fe089219b189a86c05a8555c9683be
Kirty Vijay <66775252+kivi3004@users.noreply.github.com>
2020-09-22 22:38:46 +0530

    Directed Acyclic Graph(DAG)

--- GRAPH/Topological_sort.cpp
+++ GRAPH/Topological_sort.cpp
@@ -24,6 +24,45 @@ void topologicalSort(vector<int> graph[], int n){
     }
 }
 
+void AllTopologicalRoute(vector<bool>& visit, vector<int>& res, int n, vector<int> graph[], vector<int>& indegree){
+    bool flag = false;
+    for(int u=0; u<n; u++){
+        if(indegree[u]==0 && !visit[u]){
+            for(auto k : graph[u]){
+                indegree[k]--;
+            }
+            res.push_back(u);
+            visit[u] = true;
+            AllTopologicalRoute(visit, res, n, graph, indegree);
+
+            visit[u] = false;
+            res.pop_back();
+            for(auto k : graph[u]){
+                indegree[k]++;
+            }
+            flag=true;
+        }
+    }
+    if(!flag){
+        for(int v : res)
+            cout<<v<<" ";
+        cout<<endl;
+    }
+}
+void InDegree(vector<int> graph[], vector<int>& indegree, int n){
+    for(int i=0; i<n; i++){
+        for(auto u : graph[i])
+            indegree[u]++;
+    }
+}
+void AllTopological(vector<int> graph[], int n){
+    vector<int> indegree(n);
+    InDegree(graph, indegree, n);
+    vector<bool> visit(n,false);
+    vector<int> res;
+    AllTopologicalRoute(visit, res, n, graph, indegree);
+}
+
 void add_edge(vector<int> graph[], int u, int v){
     graph[u].push_back(v);
 }
@@ -54,7 +93,8 @@ int main(){
         cin>>ch;
     }
     print(graph,n);
-    topologicalSort(graph, n);
+    topologicalSort(graph, n);    
     
-
+    cout<<"All Topological Order : \n";
+    AllTopological(graph, n);
 }
